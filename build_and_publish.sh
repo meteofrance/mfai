8aafa77ff0feb447f0151a0d82d67b30dfa46c97
colon3tocard <colon3l.tocard@gmail.com>
2024-10-24 11:32:09 +0000

    Adds dynamic versioning from git tag

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,4 +1,13 @@
 # Script to build and publish mfai using runai (MF internal docker wrapper)
+
+# Check if the repo is on a tag and if there is local modifications
+git_tag=$(git describe --tags --abbrev=0 --exact-match)
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Error: You are on a git tag but there are local modifications. Please stash them or commit, push and tag your modifications."
+    exit 1
+fi
+echo $git_tag > version.txt
+
 runai exec python -m build
 # Use first arg if provided, otherwise use testpypi
 runai exec python -m twine upload --verbose --repository ${1:-"testpypi"} dist/*

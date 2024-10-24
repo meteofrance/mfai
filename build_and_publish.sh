cec4c4748040cf9d4a7dcbcab79f4c5e81e9a3e0
colon3tocard <colon3l.tocard@gmail.com>
2024-10-24 11:34:13 +0000

    fix set

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,7 +1,7 @@
 # Script to build and publish mfai using runai (MF internal docker wrapper)
 
 # Check if the repo is on a tag and if there is local modifications
-set -x
+set -e
 git_tag=$(git describe --tags --abbrev=0 --exact-match)
 if [ -n "$(git status --porcelain)" ]; then
     echo "Error: You are on a git tag but there are local modifications. Please stash them or commit, push and tag your modifications."

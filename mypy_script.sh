6416e122f571e03279fcf29da6844cc7fcbfb64f
colon3ltocard <36170699+colon3ltocard@users.noreply.github.com>
2025-03-31 16:45:20 +0200

    Delete mypy_script.sh

--- mypy_script.sh
+++
@@ -1,6 +0,0 @@
-mypy mfai/
-  shell: /usr/bin/bash -e {0}
-  env:
-    PYTHONPATH: .
-    pythonLocation: /opt/hostedtoolcache/Python/3.10.16/x64
-    LD_LIBRARY_PATH: /opt/hostedtoolcache/Python/3.10.16/x64/lib

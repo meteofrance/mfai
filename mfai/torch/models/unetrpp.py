meteofrance/mfai

commit d7afa87a6b70d8ac16c5e80003dc63100f3a01e2

rename missed variable

mfai/torch/models/unetrpp.py
@@ -520,7 +520,7 @@ class UnetrUpBlock(nn.Module):
                 self.transp_conv = nn.Sequential(
                     nn.UpsamplingBilinear2d(scale_factor=scale_factor),
                     nn.Conv2d(
-                        in_channels, out_channels, kernel_size=kernel_size, padding=1
+                        in_channels, out_channels, kernel_size=upsample_kernel_size, padding=1
                     ),
                 )
             else:

commit 3c7af8512dcf3a23897b6f2b27ef51f12cd125b8

fix problem logic introduced in d7afa87

mfai/torch/models/unetrpp.py
@@ -517,12 +517,14 @@ class UnetrUpBlock(nn.Module):
                     upsample_kernel_size = upsample_kernel_size[:2]
                 else:
                     scale_factor = float(upsample_kernel_size)
+                if isinstance(kernel_size, tuple):
+                    kernel_size = kernel_size[:2]
                 self.transp_conv = nn.Sequential(
                     nn.UpsamplingBilinear2d(scale_factor=scale_factor),
                     nn.Conv2d(
                         in_channels,
                         out_channels,
-                        kernel_size=upsample_kernel_size,
+                        kernel_size=kernel_size,
                         padding=1,
                     ),
                 )
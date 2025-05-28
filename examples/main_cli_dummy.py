3285831c9b87c8743cd8bc01de9e72315f9eb258
Lea Berthomier <berthomier.lea@gmail.com>
2025-05-28 14:09:00 +0200

    Update main_cli_dummy.py

--- examples/main_cli_dummy.py
+++ examples/main_cli_dummy.py
@@ -4,7 +4,7 @@ from mfai.pytorch.dummy_dataset import DummyDataModule
 from mfai.pytorch.lightning_modules import SegmentationLightningModule
 
 
-def cli_main():
+def cli_main() -> None:
     cli = LightningCLI(SegmentationLightningModule, DummyDataModule)  # noqa: F841
 
 

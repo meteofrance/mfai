e07b1b389773608fa57faf1f3a8cbda4f9f6aa75
Timothy DARJO <timothy.darjo@meteo.fr>
2025-06-13 14:10:18 +0200

    fixed input shape (WIP) - test runs succesfully

--- tests/run_diffusion_test.py
+++ tests/run_diffusion_test.py
@@ -6,4 +6,4 @@ in_channels = 1
 out_channels = 1
 input_shape = (64,64)
 
-train_model(GaussianDiffusion(in_channels=in_channels,out_channels=out_channels,input_shape=input_shape),input_shape)
+train_model(GaussianDiffusion(in_channels=in_channels,out_channels=out_channels,input_shape=input_shape),(in_channels,64,64))

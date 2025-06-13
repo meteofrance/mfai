0770b369a05d84752727937c20743b0e78670f28
Timothy DARJO <timothy.darjo@meteo.fr>
2025-06-13 14:22:06 +0200

    ruff reformat

--- tests/run_diffusion_test.py
+++ tests/run_diffusion_test.py
@@ -4,6 +4,11 @@ from mfai.torch.models.gaussian_diffusion import GaussianDiffusion
 
 in_channels = 1
 out_channels = 1
-input_shape = (64,64)
+input_shape = (64, 64)
 
-train_model(GaussianDiffusion(in_channels=in_channels,out_channels=out_channels,input_shape=input_shape),(in_channels,64,64))
+train_model(
+    GaussianDiffusion(
+        in_channels=in_channels, out_channels=out_channels, input_shape=input_shape
+    ),
+    (in_channels, 64, 64),
+)

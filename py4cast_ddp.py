8f6fddab1e5f055e64dfca5618b804798666b656
Timothy DARJO <timothy.darjo@meteo.fr>
2025-08-12 11:12:01 +0200

    transferred updated py4cast_ddp file from internship-cerfacs-archive

--- py4cast_ddp.py
+++ py4cast_ddp.py
@@ -1,8 +1,11 @@
-from mfai.pytorch.models.gaussian_diffusion import GaussianDiffusion
 from typing import Any
 from py4cast.datasets import get_datasets
+import torch
+import os
 
 #loading the dataset - code from oscar
+
+SAVE_EVERY_EPOCH = True
 
 dataset_configuration: dict[str, Any] = {
     "periods": {
@@ -36,10 +39,6 @@ dataset_configuration: dict[str, Any] = {
       "file_format": "npy",
     },
     "params": {
-      "aro_t2m": {
-        "levels": [2],
-        "kind": "input_output",
-        },
       "aro_r2": {
         "levels": [2],
         "kind": "input_output",
@@ -72,28 +71,8 @@ dataset_configuration: dict[str, Any] = {
         "levels": [250, 500, 700, 850],
         "kind": "input_output",
         },
-      "arp_t": {
-        "levels": [250, 500, 700, 850],
-        "kind": "input",
-        },
-      "arp_u": {
-        "levels": [250, 500, 700, 850],
-        "kind": "input",
-        },
-      "arp_v": {
-        "levels": [250, 500, 700, 850],
-        "kind": "input"
-        },
-      "arp_z": {
-        "levels": [250, 500, 700, 850],
-        "kind": "input",
-        },
     },
 }
-
-#loading the model
-
-#TODO
 
 #extracting data from the dataset
 
@@ -108,8 +87,64 @@ train, test, val = get_datasets(
 print(train)
 print(type(train))
 
-#upscaling the data (w/ plots)
+#loading the model
 
-#TODO
+from mfai.pytorch.models.gaussian_diffusion import GaussianDiffusionSettings, GaussianDiffusion
 
-#applying the model
+settings = GaussianDiffusionSettings(
+    timesteps = 100,
+    sampling_timesteps = None,
+    objective = "pred_v",
+    beta_schedule = "sigmoid",
+    schedule_fn_kwargs = {},
+    ddim_sampling_eta = 0.0,
+    auto_normalize = True,
+    offset_noise_strength = (0.0,),
+    min_snr_loss_weight = False,
+    min_snr_gamma = 5,
+    immiscible = False
+)
+
+model = GaussianDiffusion(
+  in_channels = len(train.params), #correspond bien au nombre de features
+  out_channels = len(train.params),
+  input_shape = (train.grid.x,train.grid.y), #on part du principe que les datasets auront tous la même grille
+  settings = settings
+)
+
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+epoch_count = 1
+
+print("cuda available? ",torch.cuda.is_available())
+
+for epoch in range(epoch_count):
+    for item in train:
+        input_tensor = item.inputs.tensor.permute(0,3,1,2) #reshape to fit format
+        target_tensor = item.outputs.tensor.permute(0,3,1,2)
+        # Maintenant tu peux appeler ton modèle avec input pour générer et target pour avoir une loss
+    
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+        output = model(input_tensor)
+    
+        # Compute the loss and its gradients
+        loss = loss_fn(output, target_tensor)
+        loss.backward()
+    
+        # Adjust learning weights
+        optimizer.step()
+    
+    if SAVE_EVERY_EPOCH:
+        torch.save({
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "loss": loss.item(),
+        }, f"checkpoint_epoch_{epoch}.pth")
+torch.save({
+    "epoch": epoch,
+    "model_state_dict": model.state_dict(),
+    "optimizer_state_dict": optimizer.state_dict(),
+    "loss": loss.item(),
+}, "checkpoint_last.pth")

74423283da92b21795919b5627accea274b0ee5d
Oscar Dewasmes <oscar.dewasmes@gmail.com>
2026-07-23 09:23:48 +0000

    🔥 remove tmp file

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-
-start = dt.datetime.now()
-from mfai import *
-print(f"mfai import took {dt.datetime.now() - start}")
-
-start = dt.datetime.now()
-import mfai.pytorch.callbacks
-print(f"mfai import took {dt.datetime.now() - start}")
